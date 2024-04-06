9c6e9b76ddd99ac375b68d859e41baded48b1b71
Rajchir <r.m.choirsihabudin@student.uir.ac.id>
2024-04-06 23:25:51 +0700

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,55 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Data Diri</template>
+
+  Raja
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Hobi</template>
+
+    bermain gitar,berenang,gedrum,sepak bola dll
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Keahlian</template>
+
+    suka menunggu yang tak pasti
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>Asal </template>
+
+   Tg.balai karimun 
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Sosial media</template>
+
+    <a href="https://www.instagram.com/rajchir_/">RAJA CHOIR</a>
+  </WelcomeItem>
+</template>
